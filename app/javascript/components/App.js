492bea9c37c10986ce1ae2b303a3280b6d6224f8
Hubert MUGABO <mugberto@gmail.com>
2021-11-09 12:08:32 +0200

    Add Greeting component and routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
 
-const App = () => (<div>Hello!</div>);
+const App = () => (
+  <React.Fragment>
+    <BrowserRouter>
+    <p style={{textAlign: 'center'}}><Link to='/greet'>Click me</Link></p>
+      <Routes>
+        <Route path='/greet' element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </React.Fragment>
+);
 
-export default App;
+export default App
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Greeting = () => (
+  <p style={{textAlign: 'center'}}>Hello</p>
+);
+
+export default Greeting;
