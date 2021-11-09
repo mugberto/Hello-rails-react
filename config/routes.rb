1644e52006236bf383d5a6f4973771fb5267e025
Hubert MUGABO <mugberto@gmail.com>
2021-11-09 09:32:42 +0200

    Set up React

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static#index'
+
+  get '*page', to: 'static#index', constraints: ->(req) do 
+    !req.xhr? && req.format.html?
+  end
 end
